dcbd66e9619dfd5573f3587937a8cd69dba65040
Prabakaran <110811941+pprabakarit@users.noreply.github.com>
2023-11-05 23:03:02 +0530

    Create iam_admin_group.tf

---
+++ eks_terraform/iam_admin_group.tf
@@ -0,0 +1,10 @@
+module "eks_admins_iam_group" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "5.3.1"
+
+  name                              = "eks-admin"
+  attach_iam_self_management_policy = false
+  create_group                      = true
+  group_users                       = [module.user1_iam_user.iam_user_name]
+  custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
+}
