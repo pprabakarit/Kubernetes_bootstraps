12f93e155fde8cfa7e13dd4458ad9fe9caf7c5d9
Prabakaran <110811941+pprabakarit@users.noreply.github.com>
2023-11-05 23:01:38 +0530

    Create assume_policy_eks-admin.tf

---
+++ eks_terraform/assume_policy_eks-admin.tf
@@ -0,0 +1,19 @@
+module "allow_assume_eks_admins_iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.3.1"
+
+  name          = "allow-assume-eks-admin-iam-role"
+  create_policy = true
+
+  policy = jsonencode({
+      Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+        ]
+        Effect   = "Allow"
+        Resource = module.eks_admins_iam_role.iam_role_arn
+      },
+    ]
+  })
+}
